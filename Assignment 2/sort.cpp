43ce560ff2ab537a51225c11fd227ec637fb881d
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2021-10-18 19:07:19 -0400

    solved radix sort using counting sort

--- Assignment 2/sort.cpp
+++ Assignment 2/sort.cpp
@@ -62,10 +62,15 @@ void insertion_sort_digit(char **A, int *A_len, int l, int r, int d)
     for (int j = l + 1; j <= r; j++)
     {
         key = A[j][d];
+
+        //store the entire string instead of character
         tempString = A[j];
+
+        //store the string length
         tempLength = A_len[j];
         i = j - 1;
 
+        //ASCII comparison of the character
         while ((i >= l) && (A[i][d] > key))
         {
             A[i + 1] = A[i];
@@ -80,18 +85,61 @@ void insertion_sort_digit(char **A, int *A_len, int l, int r, int d)
 
 void counting_sort_digit(char **A, int *A_len, char **B, int *B_len, int n, int d)
 {
+    //used to store the counter on index corressponding ASCII value of the string's d
+    int c[256] = {0};
+
+    //increment the counts based on matched index with ASCII value
+    for (int i = 0; i < n; i++)
+    {
+        //need to add 1 to overcome segmentation fault
+        int asciiValue = int(A[i][d]) + 1;
+        c[asciiValue]++;
+    }
+
+    //cumulative sum
+    for (int i = 1; i < 256; i++)
+    {
+        c[i] += c[i - 1];
+    }
+
+    //based on counter values store the original strings to auxiliary storage along with lengths
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int asciiValue = int(A[i][d]) + 1;
+        B[c[asciiValue] - 1] = A[i];
+        B_len[c[asciiValue] - 1] = A_len[i];
+        c[asciiValue]--;
+    }
+
+    //retransfer the strings to original array
+    for (int i = 0; i < n; i++)
+    {
+        A[i] = B[i];
+        A_len[i] = B_len[i];
+    }
 }
 
 void radix_sort_is(char **A, int *A_len, int n, int m)
 {
-    for (int iterator = m - 2; iterator >= 0; iterator--)
+    for (int charPosition = m - 2; charPosition >= 0; charPosition--)
     {
-        insertion_sort_digit(A, A_len, 0, n - 1, iterator);
+        insertion_sort_digit(A, A_len, 0, n - 1, charPosition);
     }
 }
 
 void radix_sort_cs(char **A, int *A_len, int n, int m)
 {
+    char **auxiliaryArray;
+    int *auxiliaryLength;
+
+    //used for temporary storage
+    auxiliaryArray = new char *[n];
+    auxiliaryLength = new int[n];
+
+    for (int charPosition = m - 2; charPosition >= 0; charPosition--)
+    {
+        counting_sort_digit(A, A_len, auxiliaryArray, auxiliaryLength, n, charPosition);
+    }
 }
 
 /*
