aedfa91e7a1f213f91a133353754ff954d27da34
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2021-10-20 10:59:30 -0400

    made small changes

--- Assignment 2/random_generator.cpp
+++ Assignment 2/random_generator.cpp
@@ -15,16 +15,16 @@ void random_generator::seed(unsigned int r)
   if (initialized)
     return;
 
-/*
+  /*
  * integer part
  */
   unsigned int s;
   struct tms t_info;
 
-/*
+  /*
  * Use random device to find initial value for s
  */
-  s = r; 	/* avoid uninitialized error */
+  s = r; /* avoid uninitialized error */
   s ^= static_cast<unsigned int>(times(&t_info));
   s *= static_cast<unsigned int>(getpid());
   s += static_cast<unsigned int>(getppid());
@@ -38,21 +38,21 @@ void random_generator::seed(unsigned int r)
 /*
  * random number production via operator >>
  */
-random_generator& operator >> (random_generator& rg, int& i)
+random_generator &operator>>(random_generator &rg, int &i)
 { /*<<<*/
   i = static_cast<int>(RANDOM());
 
   return rg;
 } /*>>>*/
 
-random_generator& operator >> (random_generator& rg, long& l)
+random_generator &operator>>(random_generator &rg, long &l)
 { /*<<<*/
   l = static_cast<long>(RANDOM());
 
   return rg;
 } /*>>>*/
 
-random_generator& operator >> (random_generator& rg, double& d)
+random_generator &operator>>(random_generator &rg, double &d)
 { /*<<<*/
   double t1, t2;
 
@@ -67,25 +67,24 @@ random_generator& operator >> (random_generator& rg, double& d)
   t2 += RANDOM();
 
   if (t1 < t2)
-    d = t1/t2;
+    d = t1 / t2;
   else
-    {
-      if (t2 < t1)
-	d = t2/t1;
-      else
-	d = 0.0;
-    }
+  {
+    if (t2 < t1)
+      d = t2 / t1;
+    else
+      d = 0.0;
+  }
 
   return rg;
 } /*>>>*/
 
-
-char* random_generator::random_string(int r_max_n, int& r_n)
+char *random_generator::random_string(int r_max_n, int &r_n)
 { /*<<<*/
-  char* st;
+  char *st;
   int ch;
 
-/*
+  /*
  * detemine string size > 0 <= r_max_n
  * randomly select 0 < r_n <= r_max_n
  * allocate r_n+1 char -> set 0 from n to r_n!
@@ -95,19 +94,19 @@ char* random_generator::random_string(int r_max_n, int& r_n)
   *this >> r_n;
   r_n = (r_n % (r_max_n - 1)) + 1;
 
-  st = new char[r_n+1];
+  st = new char[r_n + 1];
 
-/*
+  /*
  * set first n characters at random from 'a' ... 'z'
  */
   for (int i = 0; i < r_n; i++)
-    {
-      *this >> ch;
+  {
+    *this >> ch;
 
-      st[i] = 'a' + static_cast<char>(ch % 26);
-    }
+    st[i] = 'a' + static_cast<char>(ch % 26);
+  }
 
-/*
+  /*
  * string end
  */
   st[r_n] = 0;
--- Assignment 2/sort.cpp
+++ Assignment 2/sort.cpp
@@ -68,6 +68,7 @@ void insertion_sort_digit(char **A, int *A_len, int l, int r, int d)
 
         //store the string length
         tempLength = A_len[j];
+
         i = j - 1;
 
         //ASCII comparison of the character
@@ -85,43 +86,46 @@ void insertion_sort_digit(char **A, int *A_len, int l, int r, int d)
 
 void counting_sort_digit(char **A, int *A_len, char **B, int *B_len, int n, int d)
 {
-    //used to store the counter on index corressponding ASCII value of the string's d
-    int c[256] = {0};
+    //used to store (auxiliary storage) the counter on index corressponding ASCII value of the string's d
+    int counter[256] = {0};
 
     //increment the counts based on matched index with ASCII value
-    for (int i = 0; i < n; i++)
+    for (int counterIterator = 0; counterIterator < n; counterIterator++)
     {
         //need to add 1 to overcome segmentation fault
-        int asciiValue = int(A[i][d]) + 1;
-        c[asciiValue]++;
+        int nextAsciiValue = int(A[counterIterator][d]) + 1;
+        counter[nextAsciiValue]++;
     }
 
     //cumulative sum
-    for (int i = 1; i < 256; i++)
+    for (int counterIterator = 1; counterIterator < 257; counterIterator++)
     {
-        c[i] += c[i - 1];
+        counter[counterIterator] += counter[counterIterator - 1];
     }
 
-    //based on counter values store the original strings to auxiliary storage along with lengths
-    for (int i = n - 1; i >= 0; i--)
+    //based on counter values store the original strings to sorted storage along with lengths
+    for (int inputIterator = n - 1; inputIterator >= 0; inputIterator--)
     {
-        int asciiValue = int(A[i][d]) + 1;
-        B[c[asciiValue] - 1] = A[i];
-        B_len[c[asciiValue] - 1] = A_len[i];
-        c[asciiValue]--;
+        int nextAsciiValue = int(A[inputIterator][d]) + 1;
+
+        B[counter[nextAsciiValue] - 1] = A[inputIterator];
+        B_len[counter[nextAsciiValue] - 1] = A_len[inputIterator];
+
+        counter[nextAsciiValue]--;
     }
 
-    //retransfer the strings to original array
-    for (int i = 0; i < n; i++)
+    //retransfer the sorted strings to original array for final result
+    for (int inputIterator = 0; inputIterator < n; inputIterator++)
     {
-        A[i] = B[i];
-        A_len[i] = B_len[i];
+        A[inputIterator] = B[inputIterator];
+        A_len[inputIterator] = B_len[inputIterator];
     }
 }
 
 void radix_sort_is(char **A, int *A_len, int n, int m)
 {
-    for (int charPosition = m - 2; charPosition >= 0; charPosition--)
+    int charPosition = m - 2;
+    for (charPosition; charPosition >= 0; charPosition--)
     {
         insertion_sort_digit(A, A_len, 0, n - 1, charPosition);
     }
@@ -129,16 +133,18 @@ void radix_sort_is(char **A, int *A_len, int n, int m)
 
 void radix_sort_cs(char **A, int *A_len, int n, int m)
 {
-    char **auxiliaryArray;
-    int *auxiliaryLength;
+    char **sortedArray;
+    int *sortedArrayLength;
 
     //used for temporary storage
-    auxiliaryArray = new char *[n];
-    auxiliaryLength = new int[n];
+    sortedArray = new char *[n];
+    sortedArrayLength = new int[n];
 
-    for (int charPosition = m - 2; charPosition >= 0; charPosition--)
+    int charPosition = m - 2;
+
+    for (charPosition; charPosition >= 0; charPosition--)
     {
-        counting_sort_digit(A, A_len, auxiliaryArray, auxiliaryLength, n, charPosition);
+        counting_sort_digit(A, A_len, sortedArray, sortedArrayLength, n, charPosition);
     }
 }
 
