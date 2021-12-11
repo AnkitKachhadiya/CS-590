36671c4cae43773ad9f4a68422533f7360503f9a
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2021-12-11 16:01:09 -0500

    formatting

--- Assignment 5/graph.h
+++ Assignment 5/graph.h
@@ -7,32 +7,33 @@ using namespace std;
 
 class Graph
 {
-    private:
-        // Total number of nodes
-        int nodes;
-        // [nodes x nodes] adjacency matrix
-        int **matrix;
-        // Helper funciton for dfs, takes a start node and a set of visited nodes as args
-        void dfs(int, set<int>*);
-        // Helper funciton for bfs, takes a start node and a set of visited nodes as args
-        void bfs(int, set<int>*);
-    public:
-        // Creates a graph of size param1
-        Graph(int);
+private:
+    // Total number of nodes
+    int nodes;
+    // [nodes x nodes] adjacency matrix
+    int **matrix;
+    // Helper funciton for dfs, takes a start node and a set of visited nodes as args
+    void dfs(int, set<int> *);
+    // Helper funciton for bfs, takes a start node and a set of visited nodes as args
+    void bfs(int, set<int> *);
 
-        // Creates a graph from an adjacency matrix of size param2
-        Graph(int**, int);
-        ~Graph();
+public:
+    // Creates a graph of size param1
+    Graph(int);
 
-        // Sets the value of the edge (param1, param2) to param3
-        // param3 is the weight of the edge
-        bool set_edge(int, int, int);
+    // Creates a graph from an adjacency matrix of size param2
+    Graph(int **, int);
+    ~Graph();
 
-        // Runs dfs
-        void dfs();
+    // Sets the value of the edge (param1, param2) to param3
+    // param3 is the weight of the edge
+    bool set_edge(int, int, int);
 
-        // Runs a bfs from the starting vertex
-        void bfs(int);
+    // Runs dfs
+    void dfs();
+
+    // Runs a bfs from the starting vertex
+    void bfs(int);
 };
 
 #endif
